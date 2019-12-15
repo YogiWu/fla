8c63bb7027ac20fbb679100687b0eacfb5357059
yogi <13710203907@163.com>
2019-12-15 20:48:12 +0800

    Updates

---
+++ precache-manifest.69414b9037c17153e624bffc36edb4c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03bd5d5e51ce0311c8954a27232ad30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "299eaa9d9f9c77efec9f",
+    "url": "./static/css/2.eebd14ee.chunk.css"
+  },
+  {
+    "revision": "c07e656dda973da0ad4c",
+    "url": "./static/css/main.617a2c3d.chunk.css"
+  },
+  {
+    "revision": "299eaa9d9f9c77efec9f",
+    "url": "./static/js/2.0d35dab5.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "./static/js/2.0d35dab5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c07e656dda973da0ad4c",
+    "url": "./static/js/main.f65be591.chunk.js"
+  },
+  {
+    "revision": "00c0371ccd788642f023",
+    "url": "./static/js/runtime-main.50283ef0.js"
+  }
+]);
