d63cb4921b755d54dffb4264f3d25dce149823ed
yogi <13710203907@163.com>
2019-12-22 21:10:15 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e08eeae4ec7d8630391c52b9b7b679b0.js"
+  "./precache-manifest.c136430f5012927a64d38006dfdae7cd.js"
 );
 
 self.addEventListener('message', (event) => {
